80369e30c3dc49bec2188d825a651cc6698d9ec1
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-15 21:28:58 +0100

    EJERCICIOS PDF HACIENDO

---
+++ Ejercicios/EjerciciosPDF/Ejercicio1PDF.java
@@ -0,0 +1,23 @@
+package EjerciciosPDF;
+
+public class Ejercicio1PDF {
+
+    public static void main(String[] args) {
+        double balance = 0;
+        balance = balance + 20; //Add quarter 1 profits (thousands).
+        balance = balance - 25; //Subtract quarter 1 expenses (thousands).
+        balance = balance + 30; //Add quarter 2 profits (thousands).
+        balance = balance - 25; //Subtract quarter 1 expenses (thousands).
+        if (balance < 0) {
+            System.out.println("We’re in the red!");
+        } else if (balance > 0) {
+            System.out.println("We made a profit!");
+        } else {
+            System.out.println("We broke even.");
+
+            //ES LA C, DEBIDO A QUE LA VARIABLE BALACE LE VAS SUMANDO Y RESTANDO VALORES Y SE QUEDA A 0
+        }
+    }
+    }
+
+
