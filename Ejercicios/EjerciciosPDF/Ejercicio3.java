aebd304646770b475e4af51a72cbbd68526810ae
Eduardo Martín-Sonseca <martinsonsecaeduardo@gmail.com>
2022-02-18 10:15:30 +0100

    HACIENOLO DE LA FORMA CORRECTA

--- Ejercicios/EjerciciosPDF/Ejercicio3.java
+++ Ejercicios/EjerciciosPDF/Ejercicio3.java
@@ -1,75 +1,18 @@
-        package EjerciciosPDF;
+package EjerciciosPDF;
 
-        public class Ejercicio3 {
+public class Ejercicio3 {
 
-                //ATTRIBUTES
+    public static void main(String[] args) {
 
-                private boolean sePuedeVerJugardor;
-                private boolean ArticuloMortal;
 
-                //CONSTRUCTORS
+        boolean sePudedeVerJugador= true;
+        boolean articuloMortal=true;
 
-                public Ejercicio3(boolean sePuedeVerJugardor, boolean articuloMortal) {
-                        this.sePuedeVerJugardor = sePuedeVerJugardor;
-                        ArticuloMortal = articuloMortal;
-                }
-
-                //METHODS GETTER AND SETTER
-
-                public boolean isSePuedeVerJugardor() {
-                        return sePuedeVerJugardor;
-                }
-
-                public void setSePuedeVerJugardor(boolean sePuedeVerJugardor) {
-                        this.sePuedeVerJugardor = sePuedeVerJugardor;
-                }
-
-                public boolean isArticuloMortal() {
-                        return ArticuloMortal;
-                }
-
-                public void setArticuloMortal(boolean articuloMortal) {
-                        ArticuloMortal = articuloMortal;
-                }
-
-                //METHODS OF UTILITY
-
-                @Override
-                public String toString() {
-                        return "Condiciones" + "sePuedeVerJugardor=" + sePuedeVerJugardor + ", ArticuloMortal=" + ArticuloMortal;
-                }
-
-                public void Ataque( boolean puedeVer, boolean derrotar) {
-
-                        if (puedeVer==true && derrotar==true){
-                                System.out.println("El enemigo puede ver al jugador");
-                                System.out.println("-------------------------------------");
-                        }
-                        else {
-                                System.out.println("El jugador puede ver al enemigo");
-
-                        }
-                }
-
-                public void huye( boolean salir, boolean derrotar){
-
-                        if (salir==true && derrotar==false ){
-                                System.out.println("El enemigo puede ver al jugador");
-                        }
-                        else {
-                                System.out.println("No puedes derrotar al enemigo");
-                        }
-                }
-
-                public void LibreDePeligro(boolean NoDetectado){
-
-                        if (NoDetectado==false){
-                                System.out.println("El jugador no esta siendo visto por el enemigo");
-                        }
-                        else {
-                                System.out.println("El jugador esta siendo visto por el enemigo");
-                        }
-                }
+        if (sePudedeVerJugador==true){
+            System.out.println("Ataque!");
+        }
+        else {
 
         }
-
+    }
+}
