f3632e88f5b660e78d6524e99fd1a9fc2d2c794e
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-19 20:41:50 +0100

    EJERCICIO 4 HECHO

--- Ejercicios/EjerciciosPDF/Ejercicio4.java
+++ Ejercicios/EjerciciosPDF/Ejercicio4.java
@@ -4,5 +4,18 @@ public class Ejercicio4 {
 
     public static void main(String[] args) {
 
+
+        //INICIALIZO LAS VARIABLES PARA UTILIZARLAS DESPUES
+        boolean estaNevando=true;
+        boolean estaLloviendo=true;
+        float Temperatura=90;
+
+        if (estaLloviendo || estaNevando==true && Temperatura <=5 ){
+
+            System.out.println("Quedate en casa");
+        }
+        else{
+            System.out.println("Vamos a salir");
+        }
     }
 }
