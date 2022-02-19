9278c0ebd5681e404a1fce8346e6a0866e7641a7
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-19 21:12:17 +0100

    EJERCICIO 6 HECHO

---
+++ Ejercicios/EjerciciosPDF/Ejercicio6.java
@@ -0,0 +1,16 @@
+package EjerciciosPDF;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        double playbackPosition = 120;
+        boolean rewinding = true;
+        if (rewinding) {
+            double rewindAmount = 0.1;
+        }
+        playbackPosition = playbackPosition - rewindAmount;
+
+        //EL ERROR ES QUE NO HAY NINGUNA VARIABLE QUE SE LLAME rewindAmount
+    }
+}
