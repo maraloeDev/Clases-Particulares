4991bfa3994dd6c14686316baf014112e3787d7e
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-20 20:00:37 +0100

    NO LO ENTIENDO

---
+++ Ejercicios/EjerciciosPDF/EjerciciosBucles/Ejercicio5.java
@@ -0,0 +1,27 @@
+package EjerciciosPDF.EjerciciosBucles;
+
+public class Ejercicio5 {
+
+    public String funcion(int n, String letra){
+
+
+//1 VERSION
+while (){
+
+
+
+
+//2 VERSION
+}
+do {
+
+}while ();
+
+
+
+
+//3 VERSIÓN
+        for ();
+
+    }
+}
