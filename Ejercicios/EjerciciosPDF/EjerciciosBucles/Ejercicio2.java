ef1772866ce7332eb3f863ead3cd0a48a1eb3e32
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-20 18:46:04 +0100

    NO LO ENTIENDO

---
+++ Ejercicios/EjerciciosPDF/EjerciciosBucles/Ejercicio2.java
@@ -0,0 +1,16 @@
+package EjerciciosPDF.EjerciciosBucles;
+
+public class Ejercicio2 {
+
+
+    public int factorial(int n){
+
+        n=5;
+
+        int total= n;
+
+        total=  n*4*3*2*1;
+
+        return factorial(5);
+    }
+}
