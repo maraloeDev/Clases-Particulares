6c53d23bc4e2cd7daf0c7dc4526a613dccae4425
Eduardo <martinsonsecaeduardo@gmail.com>
2022-02-21 23:52:18 +0100

    HACIENDO

--- Ejercicios/EjerciciosPDF/EjerciciosObjetos/Proyectos.java
+++ Ejercicios/EjerciciosPDF/EjerciciosObjetos/Proyectos.java
@@ -8,4 +8,48 @@ public class Proyectos {
     protected String nombre;
     protected String coordinador;
 
+    //CONSTRUCTORS
+
+    public Proyectos(int codigoProyecto, String nombre, String coordinador) {
+        this.codigoProyecto = codigoProyecto;
+        this.nombre = nombre;
+        this.coordinador = coordinador;
+    }
+
+    //METHODS GETTER AND SETTER
+
+    public int getCodigoProyecto() {
+        return codigoProyecto;
+    }
+
+    public void setCodigoProyecto(int codigoProyecto) {
+        this.codigoProyecto = codigoProyecto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCoordinador() {
+        return coordinador;
+    }
+
+    public void setCoordinador(String coordinador) {
+        this.coordinador = coordinador;
+    }
+
+    @Override
+    public String toString() {
+        return "Proyectos{" +
+                "codigoProyecto=" + codigoProyecto +
+                ", nombre='" + nombre + '\'' +
+                ", coordinador='" + coordinador + '\'' +
+                '}';
+    }
+
+    //METHODS OF UTILITY
 }
--- Ejercicios/EjerciciosPDF/EjerciciosObjetos/Tareas.java
+++ Ejercicios/EjerciciosPDF/EjerciciosObjetos/Tareas.java
@@ -10,4 +10,49 @@ public class Tareas {
     protected Date horasAcabo;
     protected String programador;
     protected String descripcion;
+
+    //METHODS GETTER AND SETTER
+
+    public String getAplicacion() {
+        return aplicacion;
+    }
+
+    public void setAplicacion(String aplicacion) {
+        this.aplicacion = aplicacion;
+    }
+
+    public Date getHorasAcabo() {
+        return horasAcabo;
+    }
+
+    public void setHorasAcabo(Date horasAcabo) {
+        this.horasAcabo = horasAcabo;
+    }
+
+    public String getProgramador() {
+        return programador;
+    }
+
+    public void setProgramador(String programador) {
+        this.programador = programador;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "Tareas{" +
+                "aplicacion='" + aplicacion + '\'' +
+                ", horasAcabo=" + horasAcabo +
+                ", programador='" + programador + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+    //METHODS OF UTILITY
 }
