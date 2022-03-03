0de0ca58d52616aedaccecb68e85ec044bad2168
Eduardo Martín-Sonseca <martinsonsecaeduardo@gmail.com>
2022-03-03 09:15:57 +0100

    HECHO

--- Ejercicios/EjerciciosPDF/EjerciciosMetodos/Ejercicio2.java
+++ Ejercicios/EjerciciosPDF/EjerciciosMetodos/Ejercicio2.java
@@ -2,28 +2,24 @@ package EjerciciosPDF.EjerciciosMetodos;
 
 public class Ejercicio2 {
 
-    public class Metodos_Bucles {
-
         public  int metodo1(int q){
-            return  0;
+                return  0;
         }
 
         public  int metodo2(int w){
-            return 0;
+                return 0;
         }
 
         public  int metodo3(int e){
-            return 0;
+                return 0;
         }
 
         public  int metodo4(int r){
-            return 0;
+                return 0;
         }
 
         public  int metodo5(int t){
-            return 0;
+                return 0;
         }
-
-    }
 
 }
--- Ejercicios/EjerciciosPDF/EjerciciosMetodos/Ejercicio2App.java
+++ Ejercicios/EjerciciosPDF/EjerciciosMetodos/Ejercicio2App.java
@@ -1,31 +1,79 @@
 package EjerciciosPDF.EjerciciosMetodos;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio2App {
 
-    public static Scanner teclado = new Scanner(System.in);
-
     public static void main(String[] args) {
 
-        System.out.println("Seleccione una opción");
+        Scanner teclado=new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("***** BIENVENIDO A SU BANCO *****\n" +
+                    "Seleccione una de las siguientes opciones en el teclado numérico:\n" +
+                    "1.- Consultar saldo\n" +
+                    "2.- Realizar una transferencia\n" +
+                    "3.- Consultar/cambiar los datos personales\n" +
+                    "4.- Solicitar un préstamo\n" +
+                    "5.- Pagar impuestos\n" +
+                    "0.- Cerrar sesión\n" +
+                    "*********************************");
+
+            opcion=teclado.nextInt();
+
+            switch (opcion){
+
+                case 1:
+                    System.out.println("Su saldo es de 1000 €");
+
+                    Ejercicio2 b1 = new Ejercicio2();
+                    System.out.println(b1.metodo1(1));
+                    break;
+
+                case 2:
+                    System.out.println("A quien quiere realizar la transferencia");
+
+                    Ejercicio2 b2 = new Ejercicio2();
+                    System.out.println(b2.metodo2(3));
+                    break;
+
+                case 3:
+
+                    System.out.println("Consultar/cambiar los datos personales");
+
+                    Ejercicio2 b3 = new Ejercicio2();
+                    System.out.println(b3.metodo3(7));
+                    break;
+
+                case 4:
+
+                    System.out.println("Solicitar un préstamo");
+
+                    Ejercicio2 b4 = new Ejercicio2();
+                    System.out.println(b4.metodo4(14));
+
+                    break;
+
+                case 5:
+
+                    Ejercicio2 b5 = new Ejercicio2();
+                    System.out.println(b5.metodo5(10));
+                    break;
+
+                case 0:
+                    System.out.println("“Muchas gracias por utilizar nuestros servicios. ¡Hasta\n" +
+                            "pronto!”");
+
+                    break;
+
+                default:
+                    System.out.println("Ha introducido un valor que no está en el menú");
+            }
 
 
-        System.out.println("--------------------");
-
-        System.out.println("***** BIENVENIDO A SU BANCO *****\n" +
-                "Seleccione una de las siguientes opciones en el teclado numérico:\n" +
-                "1.- Consultar saldo\n" +
-                "2.- Realizar una transferencia\n" +
-                "3.- Consultar/cambiar los datos personales\n" +
-                "4.- Solicitar un préstamo\n" +
-                "5.- Pagar impuestos\n" +
-                "0.- Cerrar sesión\n" +
-                "*********************************");
-
-        int opcion = teclado.nextInt();
+        } while (opcion>=0 || opcion <6); //Si se cumple la condicion se repite hasta que no se cumpla
 
     }
 
-
-}
+    }
