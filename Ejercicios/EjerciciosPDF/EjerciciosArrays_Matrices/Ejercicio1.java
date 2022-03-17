be0d0a64b467310a1f6da87e869c0030daa9836b
Eduardo <martinsonsecaeduardo@gmail.com>
2022-03-18 00:12:01 +0100

    PARA MOSTRAR DATOS ES
    SOUT (ARRAYS.TOSTRING(CUANTO MIDE)
    (NOMBRE DEL ARRAY)
    
    SOUT (ARRAYS.TOSTRING(NOMBRE DEL ARRAY);

---
+++ Ejercicios/EjerciciosPDF/EjerciciosArrays_Matrices/Ejercicio1.java
@@ -0,0 +1,49 @@
+package EjerciciosArrays_Matrices;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Ejercicio1 {
+
+    static String[] arrayDeAlimentos = new String[5];
+
+    public static void main(String[] args) {
+        introducirDatos();
+        mostrarDatos();
+        System.out.println(clonarArray());
+        clonarArrayV2();
+        System.out.println(agregarArrayList());
+    }
+
+    public static void introducirDatos() {
+
+        arrayDeAlimentos[0] = "Hamburguesa";
+        arrayDeAlimentos[1] = "Perrito Caliente";
+        arrayDeAlimentos[2] = "Croquetas";
+        arrayDeAlimentos[3] = "Sopa Castellana";
+        arrayDeAlimentos[4] = "Crema de calabazin";
+    }
+
+    public static void mostrarDatos() {
+
+
+        System.out.println(Arrays.toString(arrayDeAlimentos));
+    }
+
+    public static String clonarArray() {
+// Completar el método
+        return "";
+    }
+
+    public static void clonarArrayV2() {
+// Completar el método
+    }
+
+    public static String agregarArrayList() {
+
+// Completar el método
+        return "";
+    }
+}
