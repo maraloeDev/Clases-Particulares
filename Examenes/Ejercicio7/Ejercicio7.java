a60956c0932d0bdb4ae2211c49458934ecb3b76b
maraloeDev <martinsonsecaeduardo@gmail.com>
2022-04-06 10:40:57 +0200

    Haciendo ejercicios examen

---
+++ Examenes/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,24 @@
+package Ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de escalones que quieres: ");
+
+        int numero=teclado.nextInt();
+
+
+        for (int i=1; i < numero; i++ ){
+
+            int  contador=0;
+
+            System.out.println(contador + numero);
+
+
+        }
+    }
+}
