595aff465df2fcc7443c1650d1e99840cea649e1
Eduardo <martinsonsecaeduardo@gmail.com>
2022-04-08 19:08:30 +0200

    Ejercicios examen

---
+++ Examenes/Ejercicio6/src/Examen1/Ejercicio8.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Examen1;
+
+import static Examen1.Ejercicio6.teclado;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Ejercicio8 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduce un numero o salir: ");
+
+        int numeros = teclado.nextInt();
+        String frase = teclado.nextLine();
+
+        while (numeros > 0 || numeros < 0 || numeros != 0) {
+            System.out.println("Introduce un numero o salir: ");
+
+            numeros = teclado.nextInt();
+
+            System.out.println("Has introducido " + frase.toLowerCase());
+        }
+
+    }
+
+}
