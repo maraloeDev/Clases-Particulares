22da54ca97d8496daff4264fc0b8c0f07917b364
Eduardo <martinsonsecaeduardo@gmail.com>
2022-04-08 20:32:49 +0200

    Ejercicios examen

---
+++ Examenes/Ejercicio6/src/Examen1/Ejercicio9.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Examen1;
+
+import static Examen1.Ejercicio6.teclado;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Ejercicio9 {
+
+    public static void main(String[] args) {
+
+        System.out.println("introduce una frase, que la dare la vuelta");
+        String cadena = teclado.next();
+        int longitud = cadena.length();
+        for (int i = 0; i < longitud / 2; i++) {
+
+            System.out.println(cadena.charAt(i));
+
+        }
+
+    }
+
+}
