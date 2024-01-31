67aad11d799d77f82c8caf9d7c05962f614e2ff9
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2024-01-31 13:48:46 +0100

    Exmanenes

---
+++ Procesos/Examenes/src/ExamenRecuperacion/PideDatos.java
@@ -0,0 +1,15 @@
+package ExamenRecuperacion;
+
+import java.util.Scanner;
+
+public class PideDatos {
+
+    public static void main(String[] args) {
+
+        Scanner teclado=new Scanner(System.in);
+
+        System.out.print("Introduzca un número de procesos: ");
+        Integer numero=teclado.nextInt();
+    }
+
+}
---
+++ Procesos/Examenes/src/ExamenRecuperacion/ProcesoDormido.java
@@ -0,0 +1,6 @@
+package ExamenRecuperacion;
+
+public class ProcesoDormido {
+
+
+}
